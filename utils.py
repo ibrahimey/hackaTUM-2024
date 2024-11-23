6d6d2a9f5eb92b4cf8d4031b677552e393eadbac
Ibrahim <ibrahimeyurt@gmail.com>
2024-11-23 11:17:44 +0100

    adding get_news

--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
-def get_news(source='', destination='./data/news.json'):
+import feedparser
+import json
+
+def get_news(source="https://rss.app/feeds/u6rcvfy6PTSf9vQ4.xml", destination="./data/news.json"):
     """
 
-    :param source:
-    :param destination:
+    :param source: RSS feed to get the news
+    :param destination: Where to save the feed
     :return:
     """
-    pass
+    feed = feedparser.parse(source)
+
+    if feed.status == 200:
+        with open(destination, 'w') as f:
+            json.dump(feed.entries, f)
+
+    else:
+        print("Failed to get RSS feed. Status code:", feed.status)
